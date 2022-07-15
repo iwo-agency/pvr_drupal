5787d6cc3e0eaa4f0f5607dd4c1aa22c063c398b
Ivan Berezhnov <ivan@imagexmedia.comm>
2022-07-15 09:49:02 +0300

    Fix problemm with showing lang

--- web/themes/custom/pvr/js/scripts.js
+++ web/themes/custom/pvr/js/scripts.js
@@ -16,4 +16,14 @@
     }
   };
 
+  Drupal.behaviors.showLang = {
+    attach: function (context, settings) {
+      $('.block-language option').each(function () {
+        if ($(this).hasClass('is-active')) {
+          $(this).prop("selected", true);
+        }
+      });
+    }
+  };
+
 })(jQuery, Drupal, drupalSettings);
